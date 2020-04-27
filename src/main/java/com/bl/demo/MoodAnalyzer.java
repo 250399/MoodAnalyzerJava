18990e8486e35cd028a1ac2b223145bf230877c8
250399 <kunaldeshmukh2503@gmail.com>
2020-04-27 10:39:38 -0700

    ADDED: TC1.1-Sad message returns sad

--- src/main/java/com/bl/demo/MoodAnalyzer.java
+++ src/main/java/com/bl/demo/MoodAnalyzer.java
@@ -1,4 +1,11 @@
 package com.bl.demo;
 
 public class MoodAnalyzer {
+    String message;
+
+    String analyseMood(String message){
+        if(message.toLowerCase().contains("happy"))
+            return "Happy";
+        return "Sad";
+    }
 }
--- src/test/java/com/bl/demo/MoodAnalyzerTest.java
+++ src/test/java/com/bl/demo/MoodAnalyzerTest.java
@@ -1,7 +1,18 @@
 package com.bl.demo;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MoodAnalyzerTest {
+    MoodAnalyzer moodObject;
+
+    @Test
+    public void givenSadMessage_ReturnsSad(){
+        moodObject = new MoodAnalyzer();
+        Assert.assertEquals("Sad",moodObject.analyseMood("I am in Sad mood"));
+    }
 
 }
